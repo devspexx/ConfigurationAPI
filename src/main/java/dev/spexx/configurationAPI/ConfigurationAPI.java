7d62c3a57cc075a9f7b39252a1a04acee7dabc01
Spexx <janezvod0000@gmail.com>
2026-04-04 21:04:08 +0200

    refactor: remove unused config.yml and default plugin boilerplate

--- src/main/java/dev/spexx/configurationAPI/ConfigurationAPI.java
+++ src/main/java/dev/spexx/configurationAPI/ConfigurationAPI.java
@@ -1,13 +1,7 @@
 package dev.spexx.configurationAPI;
 
-import dev.spexx.configurationAPI.config.YamlConfig;
-import dev.spexx.configurationAPI.difference.ConfigLineDifference;
-import dev.spexx.configurationAPI.events.ConfigReloadedEvent;
-import dev.spexx.configurationAPI.manager.ConfigManager;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Entry point for the ConfigurationAPI plugin.
@@ -18,46 +12,6 @@ import org.jetbrains.annotations.NotNull;
  * @apiNote
  * This plugin primarily serves as a runtime host for the configuration API.
  *
- * @implSpec
- * Lifecycle methods are invoked by the Bukkit framework.
- *
  * @since 1.0
  */
-public final class ConfigurationAPI extends JavaPlugin implements Listener {
-
-    private ConfigurationAPI() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void onEnable() {
-
-        ConfigManager configManager = new ConfigManager(this);
-
-        // load test config
-        YamlConfig config = configManager.getOrLoadResource("config.yml");
-
-        // register event listener
-        getServer().getPluginManager().registerEvents(this, this);
-    }
-
-    @Override
-    public void onDisable() { }
-
-    /**
-     * Handles configuration reload events.
-     *
-     * @param event the reload event
-     *
-     * @since 1.0
-     */
-    @EventHandler
-    public void onReload(@NotNull ConfigReloadedEvent event) {
-
-        getLogger().info("Config reloaded: " +
-                event.getConfig().file().getName());
-
-        getLogger().info("New value: " +
-                event.getConfig().config().getInt("value"));
-    }
-}
+public final class ConfigurationAPI extends JavaPlugin implements Listener {}
