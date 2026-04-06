f614ac7cb88f2dedec25dd1a93059f3cbb502b9d
devspexx <spexx@users.noreply.github.com>
2026-04-06 19:56:05 +0200

    remove testing stuff

--- src/main/java/dev/spexx/configurationAPI/ConfigurationAPI.java
+++ src/main/java/dev/spexx/configurationAPI/ConfigurationAPI.java
@@ -1,12 +1,6 @@
 package dev.spexx.configurationAPI;
 
-import dev.spexx.configurationAPI.configuration.yaml.YamlConfig;
-import dev.spexx.configurationAPI.manager.ConfigManager;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.io.File;
-
 /**
  * Main plugin entry point for the ConfigurationAPI.
  *
@@ -22,26 +16,5 @@ public final class ConfigurationAPI extends JavaPlugin {
      * @since 1.0.0
      */
     public ConfigurationAPI() {
-    }
-
-    @Override
-    public void onEnable() {
-
-        ConfigManager manager = new ConfigManager(this);
-
-        File configFile = new File(new File(getDataFolder(), "sampleJar"), "config.yml");
-        manager.registerFromJar(
-                configFile,
-                "config.yml",
-                this
-        );
-
-        File customFile = new File(new File(getDataFolder(), "sampleCustom"), "data.yml");
-        YamlConfig customConfig = manager.register(customFile);
-        YamlConfiguration customYaml = customConfig.get();
-        customYaml.set("hello", "world");
-        customConfig.save();
-
-        manager.start();
     }
 }
